de15940f3b2a4681127fda920afea5f6247dee3e
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-12-12 00:40:26 +0100

    d

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -29,8 +29,8 @@ const options = {
       </span>
       </span>`,
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
       '<span class="tui-ico-ellip">...</span>' +
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
       '</a>',
   },
 };
