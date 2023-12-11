d327be0ae80fb992a640c086b2071ce7915fe728
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-12-12 00:37:24 +0100

    sdf

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -20,16 +20,15 @@ const options = {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
     currentPage:
       '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-
     moveButton: `<a href="#" class="icon tui-page-btn tui-{{type}}">
       <span class="tui-ico-{{type}}">{{type}}>
       </span>
       </a>`,
-    ellipsis: '<span class="tui-ico-ellip">...</span>', // шаблон для розділювача
     disabledMoveButton: `<span class="tui-page-btn tui-is-disabled tui-{{type}}">
       <span class="tui-ico-{{type}}">{{type}}>
       </span>
       </span>`,
+    ellipsis: '<span class="tui-ico-ellip">...</span>', // шаблон для розділювача
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' + '</a>',
   },
