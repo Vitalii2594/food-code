42bf1db860d556bc4897720c72bd496edb28aab6
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-12-12 18:38:12 +0100

    fixit css

--- src/js/actions-cards.js
+++ src/js/actions-cards.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cardsContainer = document.querySelector('.products-list-general');
-  
-    function handleScroll() {
-      const scrollPercent = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-  
-      if (scrollPercent >= 30) {
-        cardsContainer.classList.add('animation');
-      }
+  const cardsContainer = document.querySelector('.products-list-general');
+
+  function handleScroll() {
+    const scrollPercent =
+      (window.scrollY / (document.body.scrollHeight - window.innerHeight)) *
+      100;
+
+    if (scrollPercent >= 30) {
+      cardsContainer.classList.add('animation');
     }
-  
-    window.addEventListener('scroll', handleScroll);
-  });
+  }
+
+  window.addEventListener('scroll', handleScroll);
+});
