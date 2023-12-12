cb1f81fe0ac5b2bc3438bd81b91192bd92494da9
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-12-12 01:14:48 +0100

    pag

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -17,20 +17,29 @@ const options = {
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    // Шаблон для першої сторінки
+    firstPage: '<a href="#" class="tui-page-btn">{{page}}</a>',
+
+    // Шаблон для "Попередня" (можна також використовувати "prev")
     moveButton: `<a href="#" class="icon tui-page-btn tui-{{type}}">
-      <span class="tui-ico-{{type}}">{{type}}>
+      <span class="tui-ico-{{type}}">{{type}}
       </span>
       </a>`,
-    disabledMoveButton: `<span class="tui-page-btn tui-is-disabled tui-{{type}}">
-      <span class="tui-ico-{{type}}">{{type}}>
-      </span>
-      </span>`,
+
+    // Шаблон для розділювача
+    ellipsis: '<span class="tui-ico-ellip">...</span>',
+
+    // Шаблон для сторінки
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+
+    // Шаблон для поточної сторінки
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+
+    // Шаблон для "Наступна"
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
   },
 };
