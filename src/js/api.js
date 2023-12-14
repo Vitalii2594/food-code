5bc5ded805dc138e095189e81ed7cefe56f05e9e
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-12-14 09:58:09 +0100

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,38 +1,23 @@
-
-
-//запит на бекенд про категорії товарів
 import axios from 'axios';
 import { getFilter } from './filters.js';
 import storage from './local storage.js';
+import { BASE_URL } from './config.js';
 
-export const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
 const CACHE_KEY = 'cachedProductsResponse';
 
 export async function getCategories() {
-  const response = await axios.get(
-    `https://food-boutique.b.goit.study/api/products/categories`
-  );
+  const response = await axios.get(`${BASE_URL}/categories`);
   return response.data;
 }
 
-// отримання усіх продуктів при першому завантаженні
 export async function getAllProducts(queryParams) {
   let { keyword, category, page, limit, filterSearch } = queryParams;
 
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth < 768) {
-    limit = 6;
-  } else if (screenWidth >= 768 && screenWidth < 1440) {
-    limit = 8;
-  } else {
-    limit = 9;
-  }
-
   const params = new URLSearchParams({
     page,
-    limit,
+    limit: determineLimit(),
   });
+
   if (keyword !== '') {
     params.append('keyword', keyword);
   }
@@ -41,37 +26,24 @@ export async function getAllProducts(queryParams) {
     params.append('category', category);
   }
 
-  const response = await axios.get(
-    `${BASE_URL}?${params}&${getFilter(filterSearch)}`
-  );
+  const response = await axios.get(`${BASE_URL}?${params}&${getFilter(filterSearch)}`);
   return response.data;
 }
 
 export async function getDiscountProducts() {
-  const response = await axios.get(
-    `https://food-boutique.b.goit.study/api/products/discount`
-  );
+  const response = await axios.get(`${BASE_URL}/discount`);
   return response.data;
 }
 
 export async function getPopularProducts() {
-  const response = await axios.get(
-    `https://food-boutique.b.goit.study/api/products/popular`
-  );
+  const response = await axios.get(`${BASE_URL}/popular`);
   return response.data;
 }
 
-import axios from 'axios';
-import { getFilter } from './filters.js';
-import storage from './local storage.js';
-
-export const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
-const CACHE_KEY = 'cachedProductsResponse';
-
 export async function getProductsByQuery(queryParams) {
   try {
-    // Attempt to load from local storage
-    const cachedProductsResponse = storage.loadFromLocalStorage(CACHE_KEY);
+    // Spróbuj wczytać z local storage
+    const cachedProductsResponse = storage.load(CACHE_KEY);
 
     if (cachedProductsResponse) {
       return cachedProductsResponse;
@@ -92,8 +64,8 @@ export async function getProductsByQuery(queryParams) {
 
     const response = await axios.get(`${BASE_URL}?${params}&${getFilter(filterSearch)}`);
 
-    // Save to local storage
-    storage.saveToLocalStorage(CACHE_KEY, response.data);
+    // Zapisz do local storage
+    storage.save(CACHE_KEY, response.data);
 
     return response.data;
   } catch (error) {
@@ -102,43 +74,30 @@ export async function getProductsByQuery(queryParams) {
   }
 }
 
-// Rest of the code...
-
-
-export async function getProducttById(id) {
+export async function getProductById(id) {
   const response = await axios.get(`${BASE_URL}/${id}`);
+  return response.data;
+}
+
+export async function registerSubscription(client) {
+  const response = await axios.post(`${BASE_URL}/subscription`, client, {
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  return response.data;
+}
+
+export async function createOrder(order) {
+  const response = await axios.post(`${BASE_URL}/orders`, order, {
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
   return response.data;
 }
 
 function determineLimit() {
   const screenWidth = window.innerWidth;
   return screenWidth < 768 ? 6 : screenWidth < 1440 ? 8 : 9;
-}
-
-
-// POST
-export async function registrSubscription(client) {
-  const response = await axios.post(
-    `https://food-boutique.b.goit.study/api/subscription`,
-    client,
-    {
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  );
-  return response.data;
-}
-
-export async function order(order) {
-  const response = await axios.post(
-    `https://food-boutique.b.goit.study/api/orders`,
-    order,
-    {
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  );
-  return response.data;
 }
